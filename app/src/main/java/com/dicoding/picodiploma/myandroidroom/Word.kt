8e0e03f052aa8b9a15c0edd8d32e1f7c74d7fc59
yuskiah <yuskiah.pangestu45@gmail.com>
2021-04-25 21:23:35 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/picodiploma/myandroidroom/Word.kt
@@ -0,0 +1,9 @@
+package com.dicoding.picodiploma.myandroidroom
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "word_table")
+class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
